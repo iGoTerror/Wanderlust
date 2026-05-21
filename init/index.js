ee269c2cd386a357028cdd9d1507427e39d7e7f7
iGoTerror <mrdevesh05gmail.com>
2026-05-21 16:08:21 +0530

    Fix owner and Mongo issues

--- init/index.js
+++ init/index.js
@@ -20,9 +20,13 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({ ...obj, owner: '6820432b361ebb4e8fc7c7c7' }));
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: '6a0ed0dda738500b2633964c' }));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
 
 initDB();
+
+
+
+
